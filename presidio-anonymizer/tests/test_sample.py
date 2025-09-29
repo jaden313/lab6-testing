b421c3c5277866631e411cbd90ed224b78c92f99
Jaden Ziga <179839509+jaden313@users.noreply.github.com>
2025-09-29 05:28:33 +0000

    Part 3 attempt 5

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -3,13 +3,13 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
    
-   result = sample_run_anonymizer("My name is Bond.", 11, 15)
-   assert result.text == "My name is BIP."
-   assert len(result.items) == 1
-   first_item = result.items[0]
-   assert first_item.start == 11
-   assert first_item.end == 14
-   first_item_dict = first_item.to_dict()
-   assert first_item_dict["entity_type"] == "PERSON"
-   assert first_item_dict["text"] == "BIP"
-   assert first_item_dict["operator"] == "replace"
+    result = sample_run_anonymizer("My name is Bond.", 11, 15)
+    assert result.text == "My name is BIP."
+
+    assert len(result.items) == 1
+    first_item = result.items[0]
+    assert first_item.start == 11
+    assert first_item.end == 14
+    assert first_item.entity_type == "PERSON"
+    assert first_item.text == "BIP"
+    assert first_item.operator == "replace"
